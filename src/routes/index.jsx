a7c9bdb04ccd3b310afbbc7bcc5a4a22e63a5cbc
gaston.rivero <gastonrivero01@outlook.com>
2022-04-18 22:40:07 -0300

    mas arreglos

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemListContainer from "../components/itemListContainer/ItemListContainer";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
-import ItemDetailContainer from "../components/itemDetailContainer/ItemDetailContainer.jsx"
+import ItemDetailContainer from "../components/itemDetailContainer/ItemDetailContainer"
 import Cart from "../components/cart/Cart";
 import Header from "../components/header/Header";
 import Checkout from "../components/checkout/Checkout";
@@ -13,17 +13,11 @@ const Rutas = () => {
       <Header />
       <Navbar />
       <Routes>
-
         <Route path="/" element={<ItemListContainer />} />
-
         <Route path="/cart" element={<Cart />} />
-
         <Route path="/item/:id" element={<ItemDetailContainer />} />
-
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
-
         <Route path="/checkout" element={<Checkout />} />
-        
       </Routes>
       <Footer />
     </BrowserRouter>
