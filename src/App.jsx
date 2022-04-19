14b5333401b77cb608098d2d12f3ef3492258192
gaston.rivero <gastonrivero01@outlook.com>
2022-04-18 23:04:29 -0300

    modificación

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import CarritoProv from "./context/CarritoProv";
-import Rutas from "./routes";
+import Rutas from "./routes/Rutas";
 function App() {
   return (
     <div className="App">
